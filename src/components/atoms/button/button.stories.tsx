7354d0f9146ea2cf4e27fa6d7c1eef34c6965638
iriasuarez <iria.luz-suarez@and.digital>
2022-09-14 22:06:59 +0100

    added addon-action to button storybook

--- src/components/atoms/button/button.stories.tsx
+++ src/components/atoms/button/button.stories.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Button } from "./button";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "atoms/button",
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: { onClick: { action: "clicked" } },
   decorators: [
     (Story) => (
       <div style={{ margin: "10em 1em" }}>
@@ -16,16 +15,10 @@ export default {
   ],
 } as ComponentMeta<typeof Button>;
 
-const onClick = () => {
-  console.log("button Clicked ");
-};
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const Buttons = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Buttons.args = {
   primary: true,
   label: "Button very very long text here, so the button will adjust the size",
-  onClick,
 };
