4b782bc5f7bb830b9008bb7d293b8b1159c80ad3
iriasuarez <iria.luz-suarez@and.digital>
2022-09-28 19:16:02 +0100

    -add atoms and molecules to  moduleNameMappers -fix button and checkbox tests

--- src/components/atoms/button/button.test.tsx
+++ src/components/atoms/button/button.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 
-import { Button } from "./button";
+import { Button } from "@atoms";
 
 describe("Test Button component", () => {
   it("IconButton component is rendered", () => {
--- src/components/atoms/checkbox/checkbox.test.tsx
+++ src/components/atoms/checkbox/checkbox.test.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { render, screen, fireEvent, cleanup } from "@testing-library/react";
-import { Checkbox } from "./checkbox";
+import { Checkbox } from "@atoms";
 
 afterEach(cleanup);
 // When browser loaded and tests first clicked, all pass
